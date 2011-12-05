89204ed9fb685a03292e5af249275168d17c24a6
Daniel Haas <daniel.haas@childrens.harvard.edu>
2011-12-05 13:32:09 -0500

    Changed docs config to not rely on cwd of script.

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -16,8 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..')) # indivo_server/doc/sphinx
-sys.path.insert(0, os.path.abspath('../../..')) # indivo_server
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + '/../') # indivo_server/doc/sphinx
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + '/../../../') # indivo_server
 
 # -- General configuration -----------------------------------------------------
 
