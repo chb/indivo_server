2b674e75c3ef6c4b42db815dc9370a823a696e23
Daniel Haas <daniel.haas@childrens.harvard.edu>
2011-12-28 13:07:40 -0800

    re-wrote reset.sh as a python script, compatible with mysql or postgres. Moved load_codingsystems script to utils, since it shouldn't sit at the top level directory.

--- utils/importer.py
+++ utils/importer.py
@@ -1,36 +1,31 @@
-#!/usr/bin/python
-
 import os
 import sys
 from xml.dom import minidom
 import datasections
 
-verbosity = False
-if len(sys.argv[1:]) > 0 and sys.argv[1] == '-v':
-  verbosity = True
+def import_data(verbosity=True):
+  bf_prefix = ''
+  if os.path.dirname(__file__):
+    bf_prefix = os.path.dirname(__file__) + '/'
+    data_file  = bf_prefix + 'indivo_data.xml'
 
-bf_prefix = ''
-if os.path.dirname(__file__):
-  bf_prefix = os.path.dirname(__file__) + '/'
-data_file  = bf_prefix + 'indivo_data.xml'
-
-if os.path.isfile(data_file):
-  f = open(data_file)
-  lines = []
-  for line in f:
-    lines.append(line.strip())
-  dom = minidom.parseString(''.join(lines))
-  for root in dom.childNodes:
-    for section in root.childNodes:
-      try:
-        # Note the nodeName, className and fileName relationship
-        if section and hasattr(section, 'nodeName'):
-          if hasattr(datasections, section.nodeName):
-            class_name = section.nodeName.capitalize()
-            seclib = getattr(datasections, section.nodeName)
-            if hasattr(seclib, class_name):
-              getattr(seclib, class_name)(section, verbosity)
-      except ImportError:
-        pass
-else:
-  print "No indivo_data.xml file found"
+  if os.path.isfile(data_file):
+    f = open(data_file)
+    lines = []
+    for line in f:
+      lines.append(line.strip())
+    dom = minidom.parseString(''.join(lines))
+    for root in dom.childNodes:
+      for section in root.childNodes:
+        try:
+          # Note the nodeName, className and fileName relationship
+          if section and hasattr(section, 'nodeName'):
+            if hasattr(datasections, section.nodeName):
+              class_name = section.nodeName.capitalize()
+              seclib = getattr(datasections, section.nodeName)
+              if hasattr(seclib, class_name):
+                getattr(seclib, class_name)(section, verbosity)
+        except ImportError:
+          pass
+  else:
+    raise ValueError("No indivo_data.xml file found")
