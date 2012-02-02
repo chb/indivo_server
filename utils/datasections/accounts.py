361851c30bfd6a62b8dc204ef87baced5b2eb566
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-02-02 19:46:51 +0000

    Added sample data loader, integration with indivo_data.xml

--- utils/datasections/accounts.py
+++ utils/datasections/accounts.py
@@ -1,4 +1,6 @@
+import sys
 from indivo import models
+from indivo.lib.sample_data import IndivoDataLoader
 
 class Accounts:
   LABELS = 'labels'
@@ -6,15 +8,16 @@ class Accounts:
   account_tags = ('email', 'full_name', 'contact_email', 'username', 'password', 'records')
 
   def __init__(self, elements, verbosity):
-    self.process_accounts(elements, verbosity)
+    self.verbosity = verbosity
+    self.process_accounts(elements)
 
-  def process_accounts(self, elements, verbosity):
+  def process_accounts(self, elements):
     accounts = []
     for node in elements.childNodes:
       account = {}
       account_email = node.getAttribute(self.account_tags[0])
       account[self.account_tags[0]] = account_email
-      if verbosity:
+      if self.verbosity:
         print "\tAdding account: ", account_email
       for tag_name in self.account_tags:
         if tag_name == self.account_tags[-1]:
@@ -22,7 +25,8 @@ class Accounts:
           if records_node and records_node.hasChildNodes:
             account[self.LABELS] = []
             for record in records_node.childNodes:
-              account[self.LABELS].append(record.getAttribute('label'))
+              account[self.LABELS].append((record.getAttribute('label'), 
+                                           record.getAttribute('data_profile')))
         elem_node = node.getElementsByTagName(tag_name)
         if elem_node and len(elem_node) > 0 and elem_node[0].firstChild:
           account[tag_name] = elem_node[0].firstChild.nodeValue
@@ -39,8 +43,19 @@ class Accounts:
         account.set_username_and_password(  username = acct_info['username'], 
                                             password = acct_info['password'])
         if acct_info.has_key(self.LABELS):
-          for label in acct_info[self.LABELS]:
+          for label, data_profile in acct_info[self.LABELS]:
             record, record_created = models.Record.objects.get_or_create(owner = account, label = label)
             if record_created:
               record.create_default_carenets()
+
+              if data_profile:
+                if self.verbosity:
+                  print "\tLoading data profile %s for record %s"%(data_profile, label)
+                loader = IndivoDataLoader(models.NoUser.objects.get_or_create(email="", 
+                                                                              type='NoUser')[0])
+                try:
+                  loader.load_profile(record, data_profile)
+                except Exception, e:
+                  if self.verbosity:
+                    print "\t\tError loading profile: %s"%str(e)
     return True
