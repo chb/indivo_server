f3a11b0b10f962de1cdffa1ff33fc0cd2ab0d1b2
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-02-29 22:25:18 +0000

    Set up contrib data models to autoload, added a testmodel which looks a lot like Problems

--- indivo/data_models/contrib/__init__.py
+++ indivo/data_models/contrib/__init__.py
@@ -1,3 +1,13 @@
 """
 Contributed Indivo DataModels
 """
+
+import sys, os
+
+# Autoload datamodels defined in this package
+from indivo.data_models import IndivoDataModelLoader
+this_dir = os.path.abspath(os.path.dirname(__file__))
+this_module = sys.modules[__name__]
+
+loader = IndivoDataModelLoader(this_dir, module='contrib')
+loader.import_data_models(this_module)
