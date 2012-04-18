bbbd0447be70df5fcfce43e92bde2fd766b22196
Travers Franckle <travers.franckle@childrens.harvard.edu>
2012-04-18 15:29:53 -0400

    2->4 spaces

--- indivo/models/fact.py
+++ indivo/models/fact.py
@@ -5,19 +5,19 @@ from indivo.models import Record, Document
 
 class Fact(BaseModel):
 
-  id = models.CharField(max_length = 50, primary_key = True)
-  created_at = models.DateTimeField(auto_now_add = True)
-  # should we add a created_by denormalized field here to make it easier to sort facts?
-  document = models.ForeignKey(Document, related_name='allergy', null=True)
-  record = models.ForeignKey(Record, related_name='allergy', null=True)
-
-  def __unicode__(self):
-    return "%s %s" % (self.__class__.__name__, self.id)
+    id = models.CharField(max_length = 50, primary_key = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    # should we add a created_by denormalized field here to make it easier to sort facts?
+    document = models.ForeignKey(Document, related_name='allergy', null=True)
+    record = models.ForeignKey(Record, related_name='allergy', null=True)
+        
+    def __unicode__(self):
+      return "%s %s" % (self.__class__.__name__, self.id)
   
-  #Meta = BaseMeta(True)
-
-  def save(self, **kwargs):
-    if not self.id:
-      self.id = str(uuid.uuid4())
-    super(Fact, self).save(**kwargs)
+    #Meta = BaseMeta(True)
+    
+    def save(self, **kwargs):
+      if not self.id:
+        self.id = str(uuid.uuid4())
+      super(Fact, self).save(**kwargs)
 
