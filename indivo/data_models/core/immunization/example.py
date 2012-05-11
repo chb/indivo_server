9e9eab4081b0c209b50c16465a58dd3c935001fd
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-05-11 20:27:45 +0000

    Moved from old immunizations to SMART immunizations: data-model definition, examples, migrations

--- indivo/data_models/core/immunization/example.py
+++ indivo/data_models/core/immunization/example.py
@@ -2,19 +2,18 @@ from indivo.models import Immunization
 from indivo.lib.iso8601 import parse_utc_date as date
 
 immunization_fact = Immunization(
-  date_administered=date("2009-05-16T12:00:00Z"),
-  administered_by="Children's Hospital Boston",
-  vaccine_type="Hepatitis B",
-  vaccine_type_type="http://codes.indivo.org/vaccines#",
-  vaccine_type_value="123",
-  vaccine_type_abbrev="hep-B",
-  vaccine_manufacturer="Oolong Pharmaceuticals",
-  vaccine_lot="AZ1234567",
-  vaccine_expiration=date("2009-06-01"),
-  sequence=2,
-  anatomic_surface="Shoulder",
-  anatomic_surface_type="http://codes.indivo.org/anatomy/surfaces#",
-  anatomic_surface_value="234",
-  adverse_event="Pain and rash."
+    date=date("2009-05-16T12:00:00Z"),
+    administration_status_title="Not Administered",
+    administration_status_system="http://smartplatforms.org/terms/codes/ImmunizationAdministrationStatus#",
+    administration_status_identifier="notAdministered", 
+    product_class_title="TYPHOID",
+    product_class_system="http://www2a.cdc.gov/nip/IIS/IISStandards/vaccines.asp?rpt=vg#",
+    product_class_identifier="TYPHOID",
+    product_name_title="typhoid, oral",
+    product_name_system="http://www2a.cdc.gov/nip/IIS/IISStandards/vaccines.asp?rpt=cvx#",
+    product_name_identifier="25",
+    refusal_reason_title="Allergy to vaccine/vaccine components, or allergy to eggs",
+    refusal_reason_system="http://smartplatforms.org/terms/codes/ImmunizationRefusalReason#",
+    refusal_reason_identifier="allergy",
   )
 
--- indivo/data_models/core/immunization/model.py
+++ indivo/data_models/core/immunization/model.py
@@ -1,20 +1,10 @@
 from indivo.models import Fact
 from django.db import models
+from indivo.fields import CodedValueField
 
 class Immunization(Fact):
-  date_administered = models.DateTimeField(null=True)
-  administered_by   = models.CharField(max_length=40, null=True)
-  vaccine_type      =  models.CharField(max_length=200, null=True)
-  vaccine_type_type =  models.CharField(max_length=80, null=True)
-  vaccine_type_value = models.CharField(max_length=40, null=True)
-  vaccine_type_abbrev = models.CharField(max_length=20, null=True)
-  vaccine_manufacturer = models.CharField(max_length=40, null=True)
-  vaccine_lot = models.CharField(max_length=20, null=True)
-  vaccine_expiration = models.DateField(null=True)
-  sequence = models.IntegerField(null=True)
-  anatomic_surface = models.CharField(max_length=40, null=True)
-  anatomic_surface_type =  models.CharField(max_length=80, null=True)
-  anatomic_surface_value =  models.CharField(max_length=20, null=True)
-  anatomic_surface_abbrev =  models.CharField(max_length=20, null=True)
-  adverse_event = models.CharField(max_length=100, null=True)
-
+    date = models.DateTimeField(null=True)
+    administration_status = CodedValueField()
+    product_class = CodedValueField()
+    product_name = CodedValueField()
+    refusal_reason = CodedValueField()
