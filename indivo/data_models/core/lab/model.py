e4c975e274b32401a7f1f8de27088a777e9e83ec
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-05-17 18:58:32 +0000

    Moved Labs datamodel to SMART LabResults, with migration

--- indivo/data_models/core/lab/model.py
+++ indivo/data_models/core/lab/model.py
@@ -1,19 +1,15 @@
 from indivo.models import Fact
 from django.db import models
+from indivo.fields import CodedValueField, QuantitativeResultField, OrganizationField, NameField
 
-class Lab(Fact):
-  date_measured         = models.DateTimeField()
-  lab_name              = models.CharField(max_length=250, null=True)
-  lab_address           = models.CharField(max_length=250, null=True)
-  lab_type              = models.CharField(max_length=250, null=True)
-  lab_comments          = models.TextField(null=True)
-
-  first_panel_name      = models.CharField(max_length=250, null=True)
-  first_lab_test_name   = models.CharField(max_length=250, null=True)
-  first_lab_test_value  = models.CharField(max_length=250, null=True)
-
-  normal_range_minimum  = models.CharField(max_length=250, null=True)
-  normal_range_maximum  = models.CharField(max_length=250, null=True)
-  non_critical_range_minimum  = models.CharField(max_length=250, null=True)
-  non_critical_range_maximum  = models.CharField(max_length=250, null=True)
-
+class LabResult(Fact):
+    abnormal_interpretation = CodedValueField()
+    accession_number = models.CharField(max_length=255, null=True)
+    test_name = CodedValueField()
+    status = CodedValueField()
+    narrative_result = models.CharField(max_length=255, null=True)
+    notes = models.CharField(max_length=600, null=True)
+    quantitative_result = QuantitativeResultField()
+    collected_at = models.DateTimeField(null=True)
+    collected_by_org = OrganizationField()
+    collected_by_name = NameField()
